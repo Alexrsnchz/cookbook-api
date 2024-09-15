e05a3d5ddb05d586a37b5081be068f2019ef3d4f
Alexrsnchz <alexrsnchz@gmail.com>
2024-09-16 01:03:16 +0200

    Added user model + fix on package.json

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,15 @@
+class UserController {
+  static async getAllUsers(req, res) {}
+
+  static async getUserById(req, res) {}
+
+  static async register(req, res) {}
+
+  static async login(req, res) {}
+
+  static async updateUser(req, res) {}
+
+  static async deleteUser(req, res) {}
+}
+
+export default UserController;
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+import prisma from '../../prisma.js';
+
+class User {
+  static async getAll() {
+    return prisma.user.findMany();
+  }
+
+  static async getById(id) {
+    return prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  static async getByEmail(email) {
+    return prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  static async create(data) {
+    return prisma.user.create({
+      data: data,
+    });
+  }
+
+  static async update(id, data) {
+    return prisma.user.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  static async delete(id) {
+    return prisma.user.delete({
+      where: { id },
+    });
+  }
+}
+
+export default User;
